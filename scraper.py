045298c9c1f40d22cc307f94444586d1e486ce42
RinkeshR00 <54661477+RinkeshR00@users.noreply.github.com>
2020-02-21 13:10:45 +0530

    Update scraper.py
    Scrape Members from Group

--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ client = TelegramClient(phone, api_id, api_hash)
 client.connect()
 if not client.is_user_authorized():
     client.send_code_request(phone)
-    client.sign_in(phone, input('Enter the code: '))
+    client.sign_in(phone, input('verification Code enter kar: '))
  
  
 chats = []
@@ -35,20 +35,20 @@ for chat in chats:
     except:
         continue
  
-print('Choose a group to scrape members from:')
+print('choose a group to scrape members from:')
 i=0
 for g in groups:
     print(str(i) + '- ' + g.title)
     i+=1
  
-g_index = input("Enter a Number: ")
+g_index = input("Number Enter kar: ")
 target_group=groups[int(g_index)]
  
 print('Fetching Members...')
 all_participants = []
 all_participants = client.get_participants(target_group, aggressive=True)
  
-print('Saving In file...')
+print('member.csv me save ho raha hai...')
 with open("member.csv","w",encoding='UTF-8') as f:
     writer = csv.writer(f,delimiter=",",lineterminator="\n")
     writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
@@ -67,4 +67,4 @@ with open("member.csv","w",encoding='UTF-8') as f:
             last_name= ""
         name= (first_name + ' ' + last_name).strip()
         writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
-print('Members scraped successfully by @Wrong_User_Name')
+print('Gandu Members scrape ho gaye hai. By @Wrong_User_Name')
