ae5edf603b7a3579963c57a8c89415bacb2d8638
RinkeshR00 <54661477+RinkeshR00@users.noreply.github.com>
2020-02-21 14:02:20 +0530

    Update adder.py

--- adder.py
+++ adder.py
@@ -16,7 +16,7 @@ client = TelegramClient(phone, api_id, api_hash)
 client.connect()
 if not client.is_user_authorized():
     client.send_code_request(phone)
-    client.sign_in(phone, input('Enter the code: '))
+    client.sign_in(phone, input('Gandu Verification Code De: '))
  
 input_file = sys.argv[1]
 users = []
@@ -52,7 +52,7 @@ for chat in chats:
     except:
         continue
  
-print('Group choose kar jisme add karne hai:')
+print('Choose a group to add members:')
 i=0
 for group in groups:
     print(str(i) + '- ' + group.title)
@@ -77,10 +77,10 @@ for user in users:
         else:
             sys.exit("Invalid Mode Selected. Please Try Again.")
         client(InviteToChannelRequest(target_group_entity,[user_to_add]))
-        print("Thodi der ruk jaa. Script by @Wrong_User_Name...")
-        time.sleep(10)
+        print("Gandu 20 Seconds Ruk Jaa...")
+        time.sleep(20)
     except PeerFloodError:
-        print("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
+        print("Getting Flood Error from telegram. Script is stopping now. Please try again after some time. @Wrong_User_Name")
     except UserPrivacyRestrictedError:
         print("The user's privacy settings do not allow you to do this. Skipping.")
     except:
